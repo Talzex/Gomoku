2af3bf4ec487e41701edec43cade3d5e333795e5
Julian Forme <julian.forme@etu.u-bordeaux.fr>
2021-03-23 11:00:46 +0100

    game humanplayer

--- Gomoku/src/Gomoku/Match.java
+++ Gomoku/src/Gomoku/Match.java
@@ -10,5 +10,17 @@ package Gomoku;
  * @author tduthil/jforme
  */
 public class Match {
+    int largeur;
+    int hauteur;
+    Player joueur1;
+    Player joueur2;
     
+    Match(int largeur, int hauteur, Player joueur1, Player joueur2){
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+        this.joueur1 = joueur1;
+        this.joueur2 = joueur2;
+    }
+    
+    Match m = new Match(20,20,new HumanPlayer("Anna"), new HumanPlayer("Bob")) {};
 }
