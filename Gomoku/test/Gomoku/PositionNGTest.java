1b2fa837703882260941823eec7adc3a55d681e1
Thomas Duthil <tduthil@IUT.BX1>
2021-03-29 11:40:16 +0200

    Ajout Position Test

---
+++ Gomoku/test/Gomoku/PositionNGTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gomoku;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author tduthil
+ */
+public class PositionNGTest {
+
+    /**
+     * Test of getCol method, of class Position.
+     */
+    @Test
+    public void testGetCol() {
+        System.out.println("getCol");
+        Position instance = new Position(3,4);
+        int expResult = 3;
+        int result = instance.getCol();
+        assertEquals(result, expResult);
+        
+        System.out.println("getColLettre");
+        /*Position instanceLettre = new Position("A4");*/
+    }
+
+    /**
+     * Test of getLig method, of class Position.
+     */
+    @Test
+    public void testGetLig() {
+        System.out.println("getLig");
+        Position instance = new Position(3,4);
+        int expResult = 4;
+        int result = instance.getLig();
+        assertEquals(result, expResult);
+    }
+    
+}
